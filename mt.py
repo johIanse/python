136c214fbef65623a458612a4e54ded63d3d557f
johIanse <119712859+johIanse@users.noreply.github.com>
2023-04-30 20:22:17 +0800

    蜜堂APP修复有多个签到只签到第一个的问题

--- mt.py
+++ mt.py
@@ -13,7 +13,6 @@ url1 = 'https://api.mitangwl.cn/app/my/queryMyApprointmentList'
 url2 = 'https://api.mitangwl.cn/app/my/appointmentSign'
 url3 = 'https://api.mitangwl.cn/app/my/queryUserBalance'
 
-
 headers = {
     'Cookie': mt_ck
 }
@@ -24,19 +23,20 @@ response = requests.post(url=url1, headers=headers, json=data)
 
 response_json = response.json()
 
-user_list = response_json['data']['list'][0]['userList']
-for user in user_list:
-    nick_name = user['nickName']
-    print(nick_name)
+for appointment in response_json['data']['list']:
+    user_list = appointment['userList']
+    for user in user_list:
+        nick_name = user['nickName']
+        print(nick_name)
 
-data2 = {"appointmentId": int(response_json['data']['list'][0]['appointmentId'])}
+    data2 = {"appointmentId": int(appointment['appointmentId'])}
 
-response2 = requests.post(url=url2, headers=headers, json=data2)
+    response2 = requests.post(url=url2, headers=headers, json=data2)
 
-response_json = json.loads(response2.content)
+    response_json = json.loads(response2.content)
 
-msg = response_json.get('msg')
-print(msg)
+    msg = response_json.get('msg')
+    print(msg)
 
 response3 = requests.post(url3, headers=headers)
 content_str = response3.content.decode('utf-8')
