91783085344accb817be0c4007c6765a663b41ae
johIanse <119712859+johIanse@users.noreply.github.com>
2023-10-25 22:57:51 +0800

    Add files via upload
    MT论坛签到

--- mt.py
+++ mt.py
@@ -1,76 +1,113 @@
+'''
+变量 MT_BBS 
+值   账号;密码【英文的逗号;】
+cron:  0 8 * * *
+new Env('MT论坛签到')
+'''
 import requests
-import json
+import re
 import os
-from time import sleep
+from notify import send
 
-mt_ck = os.environ.get('mt_ck')
+proxies = {
+    "http": "http://180.101.50.208:443",
+    "https": "http://180.101.50.208:443",
+}
 
-if mt_ck:
-    mt_ck_list = mt_ck.split('#')
-    
-    for mt_ck_str in mt_ck_list:
-        mt_ck_values = mt_ck_str.split('&')
-        ck = mt_ck_values[0].split('=')[1]
-        zfb = mt_ck_values[1] if len(mt_ck_values) > 1 else ''
-        xm = mt_ck_values[2] if len(mt_ck_values) > 2 else ''
+bbs_url = "https://bbs.binmt.cc/member.php"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.50'}
+session = requests.session()
+def getLoginHashes():
+    params = {
+        'mod': 'logging',
+        'action': 'login'
+    }
+    login_res = session.get(url=bbs_url,headers=headers,params=params,proxies=proxies)
+    try:
+        loginhash = re.search(r'loginhash=(.+?)"', login_res.text).group(1)
+    except:
+        print("登录loginhash查找失败，退出")
+        return False
+    try:
+        formhash = re.search(r'name="formhash" value="(.+?)"', login_res.text).group(1)
+    except:
+        print("登录formhash查找失败，退出")
+        return False
+    return loginhash,formhash
 
-#        print(xm, zfb, ck)
+def login(loginhash,formhash,u,p,loginfield = "username"):
+    params = {
+        'mod': 'logging',
+        'action': 'login',
+        'loginsubmit': 'yes',
+        'loginhash': loginhash,
+        'inajax': '1'
+    }
+    data = {
+        'formhash': formhash,
+        'loginfield': loginfield,
+        'username': u,
+        'password': p,
+        'questionid': '0',
+        'answer': ''
+    }
+    res = session.post(url=bbs_url,headers=headers,params=params,data=data,proxies=proxies)
+    if '欢迎您回来' in res.text:
+        print('登录成功')
+    elif "手机号登录成功" in res.text:
+        print('手机号登录成功')
+    else:
+        print("登录失败\n",res.text)
+        return False
 
+def checkin():
+    checkin_res = session.get(url='https://bbs.binmt.cc/k_misign-sign.html',headers=headers,proxies=proxies)
+    try:
+        checkin_formhash = re.search('name="formhash" value="(.+?)"',checkin_res.text).group(1)
+    except:
+        return "签到formhash查找失败，退出"
+    res= session.get(f'https://bbs.binmt.cc/plugin.php?id=k_misign%3Asign&operation=qiandao&format=empty&formhash={checkin_formhash}',headers=headers,proxies=proxies)
+    if "![CDATA[]]" in res.text:
+        return '🎉签到成功'
+    elif "今日已签" in res.text:
+        return '🐔今日已签'
+    else:
+        print(res.text)
+        return '签到失败'
 
-        url1 = "https://api.mitangwl.cn/app/my/queryMyApprointmentList"
-        url2 = "https://api.mitangwl.cn/app/my/appointmentSign"
-        url3 = "https://api.mitangwl.cn/app/my/queryUserBalance"
-        url4 = "https://api.mitangwl.cn/app/my/applyCashOut"
+def checkinfo():
+    res = session.get(url='https://bbs.binmt.cc/k_misign-sign.html',headers=headers,proxies=proxies)
+    user = re.search('class="author">(.+?)</a>',res.text).group(1)
+    lxdays = re.search('id="lxdays" value="(.+?)"',res.text).group(1)
+    lxlevel = re.search('id="lxlevel" value="(.+?)"',res.text).group(1)
+    lxreward = re.search('id="lxreward" value="(.+?)"',res.text).group(1)
+    lxtdays = re.search('id="lxtdays" value="(.+?)"',res.text).group(1)
+    paiming = re.search('您的签到排名：(.+?)<',res.text).group(1)
+    msg = f'【MT论坛账号】{user}\n【连续签到】{lxdays}\n【签到等级】Lv.{lxlevel}\n【积分奖励】{lxreward}\n【签到天数】{lxtdays}\n【签到排名】{paiming}\n\n'
+    return msg
 
-
-        headers = {
-           'Cookie': 'JSESSIONID=' + ck
-        }
-
-#        print(headers)
-        data = {}
-
-        response = requests.post(url=url1, headers=headers, json=data)
-
-        response_json = response.json()
-
-        for appointment in response_json['data']['list']:
-            user_list = appointment['userList']
-            for user in user_list:
-                nick_name = user['nickName']
-                print("拼团用户 : " + nick_name)
-
-            data2 = {"appointmentId": int(appointment['appointmentId'])}
-
-            sleep(120)
-            response2 = requests.post(url=url2, headers=headers, json=data2)
-
-            response_json = json.loads(response2.content)
-
-            msg = response_json.get('msg')
-            print(msg)
-
-        response3 = requests.post(url3, headers=headers)
-        content_str = response3.content.decode('utf-8')
-        content_json = response3.json()
-        amount = content_json['data']['amount']
-        print("账户余额 " + str(amount))
-
-
-        payload = {
-            'alipayAccount': zfb,
-            'alipayName': xm,
-        }
-
-
-        sleep(120)
-        response4 = requests.post(url4, json=payload, headers=headers)
-        if response.status_code == 200:
-           print(response4.json()['msg'])
+if __name__ == "__main__":
+    if 'MT_BBS' in os.environ:
+        print("###MT论坛签到###")
+        config = os.environ['MT_BBS'].split(';')
+        username = config[0]
+        password = config[1]
+        hashes = getLoginHashes()
+        if hashes is False:
+            msg = 'hash获取失败'
         else:
-           print("请求失败，状态码为：" + str(response.status_code))
-else:
-    print('请设置环境变量 mt_ck')
-    exit()
-    
-    
+            if "@" in username:
+                loginfield = "email"
+            else:
+                loginfield = "username"
+            if login(hashes[0],hashes[1],username,password,loginfield) is False:
+                msg = '账号登录失败'
+                print(f'{username}\n{password}')
+            else:
+                c = checkin()
+                info = checkinfo()
+                msg = info + c
+        # 青龙通知推送
+        send('MT论坛签到',msg)
+    else:
+        print('未添加"MT_BBS"变量，退出')
